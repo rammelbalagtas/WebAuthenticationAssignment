188f9ee7402270244cfb7b2e9b6e8d04db56e8f9
Rammel <rammel.balagtas@gmail.com>
2022-03-21 15:14:02 -0400

    Add a new Secret page under the Test controller
    Add an empty view under Views/Test folder. Add a new method called Secret inside the Test controller that will return this new page.

--- WebAuthenticationAssignment/Controllers/TestController.cs
+++ WebAuthenticationAssignment/Controllers/TestController.cs
@@ -12,5 +12,10 @@ namespace WebAuthenticationAssignment.Controllers
         {
             return View();
         }
+
+        public IActionResult Secret()
+        {
+            return View();
+        }
     }
 }
